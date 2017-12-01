b74acb2a08bb8b9dfafc7eb60448ec2cff61955b
Nick Foden <nickfoden@gmail.com>
2017-12-01 14:01:24 -0500

    Adding config

--- server.js
+++ server.js
@@ -3,3 +3,48 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+
+
+const {PORT, DATABASE_URL, CLOUDINARY_URL, CLIENT_ORIGIN} = require('./config');
+
+app.use(bodyParser.json());
+
+let server;
+
+function runServer(databaseUrl=DATABASE_URL, port=PORT){
+	return new Promise((resolve, reject) => {
+    	mongoose.connect(databaseUrl, err => {
+			if (err) {
+				return reject(err);
+			}
+			server = app.listen(port, () => {
+				console.log(`Your app is listening on port ${port}`);
+				resolve();
+	      	})
+	      	.on('error', err => {
+	        	mongoose.disconnect();
+	        	reject(err);
+      		});
+    	});
+  	});
+}
+
+function stopServer() {
+  return mongoose.disconnect().then(() => {
+     return new Promise((resolve, reject) => {
+       console.log('Closing server');
+       server.close(err => {
+           if (err) {
+               return reject(err);
+           }
+           resolve();
+       });
+     });
+  });
+}
+
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+};
+
+module.exports = {app, runServer, stopServer};
