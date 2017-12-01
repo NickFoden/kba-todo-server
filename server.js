2cdd637adcb4f6dce8e4c039b24d9d2909473f7d
Nick Foden <nickfoden@gmail.com>
2017-12-01 14:03:50 -0500

    Added Cors.

--- server.js
+++ server.js
@@ -3,11 +3,32 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 
-const {PORT, DATABASE_URL, CLOUDINARY_URL, CLIENT_ORIGIN} = require('./config');
+const {PORT, DATABASE_URL, CLIENT_ORIGIN} = require('./config');
 
 app.use(bodyParser.json());
+
+
+//CORS
+
+app.use(cors({
+    origin: CLIENT_ORIGIN
+}))
+
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
+  next();
+});
+
+//Server
 
 let server;
 
